3d3b3e8f7f6ad2d7db8ce533baf8aed01cc30ba6
nietup <jakub.nietupski@gmail.com>
2022-02-16 19:12:24 +0100

    rename test for WaitNotifyExample

--- src/test/java/com/example/thread/WaitNotifyExampleTests.java
+++ src/test/java/com/example/thread/WaitNotifyExampleTests.java
@@ -6,7 +6,7 @@ import static java.lang.Thread.sleep;
 
 public class WaitNotifyExampleTests {
     @Test
-    void test() throws InterruptedException {
+    void whenGuardedBlockIsPresent_thenCodeWontBeExecutedUntilConditionIsFulfilled() throws InterruptedException {
         var example = new WaitNotifyExample();
         new Thread(example::awaitCondition).start();
         sleep(1000);
